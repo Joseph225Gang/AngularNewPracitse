901d86367ce62297fed001f348d370a263b16c21
Joseph <lai225joseph@hotmail.com>
2023-03-05 16:59:42 +0800

    standalone routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StandaloneComponent } from './standalone/standalone.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "standalone",
+    component: StandaloneComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
